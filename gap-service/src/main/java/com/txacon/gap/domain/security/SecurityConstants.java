001163732c2ad7829d19c90ee88aff18304524e2
Antonio <antonio.dm.chacon@gmail.com>
2021-03-27 20:40:44 +0100

    Fix casting bug

--- gap-service/src/main/java/com/txacon/gap/domain/security/SecurityConstants.java
+++ gap-service/src/main/java/com/txacon/gap/domain/security/SecurityConstants.java
@@ -4,7 +4,7 @@ public final class SecurityConstants {
 
   public static final String LOGIN_URL = "/login";
   public static final String ISSUER_INFO = "http://gap.es";
-  public static final long TOKEN_EXPIRATION_TIME = 60 * 60 * 1000;
+  public static final long TOKEN_EXPIRATION_TIME = 60L * 60L * 1000L;
   public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
   public static final String TOKEN_BEARER_PREFIX = "Bearer ";
   public static final String AUTHORITY_CLAIMS = "authorities";
